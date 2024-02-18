a7c34d7e7f77d32fd75fc628e5e17b58cf6c85a6
Damaride Putignano <damarideputignano@gmail.com>
2024-02-18 20:37:12 +0100

    Added responsive media queries

--- base_di_partenza/homepage.js
+++ base_di_partenza/homepage.js
@@ -1,3 +1,6 @@
+/* 
+PRIMO METODO: CAMBIO COLORE DI SFONDO TRAMITE ALTEZZA DELLO SCROLL SULL'ASSE Y
+
 const navbar = document.querySelector(".nav-container");
 const button = document.querySelector("nav button")
 window.onscroll = () => {
@@ -9,3 +12,36 @@ window.onscroll = () => {
     button.classList.remove("green-button")
   }
 };
+ */
+
+
+
+// SECONDO METODO PIU' RESPONSIVE: CAMBIO COLORE DI SFONDO AL RAGGIUNGIMENTO
+// DELLA FINE DELL'HEADER
+
+
+
+const nav = document.querySelector(".nav-container");
+const header = document.querySelector("header");
+const button = document.querySelector("nav button")
+const navHeight = nav.getBoundingClientRect().height;
+
+function updateNavColor(entries) {
+  const [entry] = entries;
+  if(!entry.isIntersecting) {
+    nav.classList.add("nav-white");
+    button.classList.add("green-button");
+  } else {
+    nav.classList.remove("nav-white");
+    button.classList.remove("green-button")
+  }
+  
+}
+
+const headerObserver = new IntersectionObserver(updateNavColor, {
+  root: null,
+  threshold: 0,
+  rootMargin: `-${navHeight}px`
+});
+
+headerObserver.observe(header)
